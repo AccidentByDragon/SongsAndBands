27a4cd558c5387fec95adc90069be09648ed936e
AccidentByDragon <karl17loe@hotmail.com>
2023-10-13 13:46:19 +0200

    more setup

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 import Band from "./band.js";
 import Musician from "./musician.js";
 import PromptSync from "prompt-sync";
+import data from "./data.json";
 const prompt = PromptSync({ sigint: true });
+import fs from "fs";
+
+
