4ae29811b7d437d9b9e67293271a845f25ac1307
AccidentByDragon <karl17loe@hotmail.com>
2023-10-16 14:21:23 +0200

    fixes and updates
    Finished the main portion of adding bands and musicians and enabled the entered data to be stored in the JSON file.

--- main.js
+++ main.js
@@ -7,18 +7,6 @@ const jsonstring = fs.readFileSync("data.json");
 const bandsAndMusiciansData = JSON.parse(jsonstring);
 console.log(bandsAndMusiciansData);
 
-console.log(`Please enter the following pieces of data:`);
-let nameOfBand = prompt(`the name of the band: `);
-let bandFounding = prompt(`the year the band was formed: `);
-let newBand = new Band(nameOfBand, bandFounding);
-let newBandInfo = prompt(`Please enter some information about the band: `)
-newBand.addInfo(newBandInfo);
-bandsAndMusiciansData.bands[bands.length] = {
-  "Band name: ": newBand.nameBand,
-  "Band Founding: ": newBand.yearFounding,
-  "Band Info:": newBand.infoTextBand
-}
-
 let isRunning = true;
 
 while (isRunning === true) {
@@ -40,25 +28,40 @@ while (isRunning === true) {
       let newBand = new Band(nameOfBand, bandFounding);
       let newBandInfo = prompt(`Please enter some information about the band: `)
       newBand.addInfo(newBandInfo);
-      bandsAndMusiciansData.bands[bands.length] = {
+      bandsAndMusiciansData.bands[bandsAndMusiciansData.bands.length] = {
         "Band name: ": newBand.nameBand,
         "Band Founding: ": newBand.yearFounding,
         "Band Info:": newBand.infoTextBand,
       }
+      fs.writeFile('data.json', JSON.stringify(bandsAndMusiciansData, null, 2), (err) => {
+        if (err) throw err;
+        console.log(`data written to file`)
+      });
       break;
     
     case 2:
       console.log(`Please enter the following pieces of data:`);
       let nameOfArtist = prompt(`the name of the musician: `);
-      let dateOfBirt = prompt(`the year the artist was born: `);
+      let dateOfBirt = Number(prompt(`the year the artist was born as a number: `));
       let artistsRoles = prompt(`the roles/instruments the musician plays: `);
       let newArtist = new Musician(nameOfArtist, dateOfBirt, artistsRoles);
       let newMusicianInfo = prompt(`Please enter some information about the musician: `);
       newArtist.addInfoMusician(newMusicianInfo);
+      let yearForMaths = new Date().getFullYear();
+      bandsAndMusiciansData.musicians[bandsAndMusiciansData.musicians.length] = {
+        "Musicians name: ": newArtist.nameMusician,
+        "Musicians Age: ": yearForMaths - newArtist.birthYear,
+        "Musician Info:": newArtist.infoTextMusician
+      }
+      fs.writeFile('data.json', JSON.stringify(bandsAndMusiciansData, null, 2), (err) => {
+        if (err) throw err;
+        console.log(`data written to file`)
+      });
       break;
     
     case 3:
-
+      let editObject = prompt(`Please enter one fo the following to edit: Band or Musician`).trim().toLocaleLowerCase();
+      EditingEntry(editObject);
       break;
     
     case 4:
@@ -75,7 +78,16 @@ while (isRunning === true) {
   }
 }
 
-function EditingEntry() {
-  
+function EditingEntry(thingToEdit) {
+  if (thingToEdit === "band") {
+    
+  }
+  else if (thingToEdit === "musician" || thingToEdit === "artist") {
+    
+  }
+  else {
+    console.log(`The object you wish to enter is not a valid entry, please try again`);
+    console.log(`returning to the menu`);
+  }
 }
 
